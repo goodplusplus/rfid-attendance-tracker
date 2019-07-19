1ddf590fa1bda9b2ba634a4ed73000a0c60aaabb
Sean Cortes <seancortes@gmail.com>
2019-07-18 21:25:00 -0700

    implement store example

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -6,12 +6,17 @@
 
 <script>
 
+import { loginStore } from '../store/loginStore';
+
 const API_URL = 'http://localhost:8081/';
 
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      msg: "Hello World!",
+      data: loginStore.data
+    };
   },
   mounted() {
     fetch(API_URL)
---
+++ client/src/store/loginStore.js
@@ -0,0 +1,3 @@
+export const loginStore = {
+  data: 1
+}
