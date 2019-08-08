27f1ae6a99a9943402b9e2abd552869d6601d64c
Sean Cortes <seancortes@gmail.com>
2019-08-07 19:50:55 -0700

    connect database

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import { json } from 'body-parser';
 import cors from 'cors';
+// import * as pgp from 'pg-promise';
+
+import { connection } from '../dbcon';
+
+const pgp = require('pg-promise')();
 
 const port = 8081;
+const db = pgp(connection);
 
 const app = express();
 app.use(json());
 app.use(cors());
+
+db.one('SELECT * FROM login')
+  .then(res => console.log(res));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello Good++!' });
